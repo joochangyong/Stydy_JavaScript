c93c7d0df1ceecf3ec36a90672fd14ce05672f3c
joochangyong <wnckddyd0525@gmail.com>
2020-12-20 23:43:54 +0900

    let, const

--- VainillaJavaScript/js/index.js
+++ VainillaJavaScript/js/index.js
@@ -1,1 +1,13 @@
-console.log('Im working. im Js. Im Beautivul. Im worth it');
+// let : 변수를 초기화 하거나 생성할때 사용, 값을 바꿀 수 있음
+let a = 221;
+let b = a - 5;
+a = 4;
+console.log(b, a);
+console.log('b =' +  b, 'a =' + a);
+console.log(`b = ${b} a = ${a}`);
+
+// const : let와 마찬가지로 변수를 초기화 하거나 생성할때 사용, 단 값을 바꿀 수 없음!
+const c = 221;
+const d = c - 5;
+
+console.log(`d = ${d} c = ${c}`);
