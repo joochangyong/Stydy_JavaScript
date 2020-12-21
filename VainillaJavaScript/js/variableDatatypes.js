50cd7e6b93f053bad0290224a4dce1d0bbd41f52
joochangyong <wnckddyd0525@gmail.com>
2020-12-21 22:21:31 +0900

    정리

--- VainillaJavaScript/js/variableDatatypes.js
+++ VainillaJavaScript/js/variableDatatypes.js
@@ -1,44 +1,51 @@
-/* let : 변수를 초기화 하거나 생성할때 사용, 값을 바꿀 수 있음
+// let : 변수를 초기화 하거나 생성할때 사용, 값을 바꿀 수 있음
+console.log("let------------------------------------");
 let a = 221;
 let b = a - 5;
 a = 4;
 console.log(b, a);
 console.log('b =' +  b, 'a =' + a);
 console.log(`b = ${b} a = ${a}`);
-*/
 
-/* const : let와 마찬가지로 변수를 초기화 하거나 생성할때 사용, 단 값을 바꿀 수 없음!
+
+//const : let와 마찬가지로 변수를 초기화 하거나 생성할때 사용, 단 값을 바꿀 수 없음!
+console.log("const------------------------------------");
 const c = 221;
 const d = c - 5;
-
 console.log(`d = ${d} c = ${c}`);
-*/
 
-/* String
-const what = "changyong";
-console.log(what);
-*/
 
-/* Boolean
-const wat = false;
-*/
+// String
+console.log("String------------------------------------");
+const String = "changyong";
+console.log(String);
 
-/* Number
-const wat = 6666;
-*/
 
-/* float
-const wat = 55.1;
-*/
+// Boolean
+console.log("Boolean------------------------------------");
+const Boolean = false;
+console.log(Boolean);
 
-/* Array
+// Number
+console.log("Number------------------------------------");
+const Number = 6666;
+console.log(Number);
+
+// float
+console.log("float------------------------------------");
+const float = 55.1;
+console.log(float);
+
+// Array
+console.log("Array------------------------------------");
 const something = "something"
 const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", true, something];
 console.log(daysOfWeek);
 console.log(daysOfWeek[2]);
-*/
 
-/* Object
+
+// Object
+console.log("Object------------------------------------");
 const userInfo = {
     name: "changyong",
     age: 25,
@@ -59,4 +66,3 @@ console.log(userInfo.age);
 userInfo.age=26;
 console.log(userInfo.age);
 console.log(userInfo.name +" is "+ userInfo.age);
-*/
