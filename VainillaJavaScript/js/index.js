40177540627660b53adb0ea9d2e78a3a5ffbefbd
joochangyong <wnckddyd0525@gmail.com>
2020-12-21 21:11:39 +0900

    Array

--- VainillaJavaScript/js/index.js
+++ VainillaJavaScript/js/index.js
@@ -1,16 +1,18 @@
-// let : 변수를 초기화 하거나 생성할때 사용, 값을 바꿀 수 있음
+/* let : 변수를 초기화 하거나 생성할때 사용, 값을 바꿀 수 있음
 let a = 221;
 let b = a - 5;
 a = 4;
 console.log(b, a);
 console.log('b =' +  b, 'a =' + a);
 console.log(`b = ${b} a = ${a}`);
+*/
 
-// const : let와 마찬가지로 변수를 초기화 하거나 생성할때 사용, 단 값을 바꿀 수 없음!
+/* const : let와 마찬가지로 변수를 초기화 하거나 생성할때 사용, 단 값을 바꿀 수 없음!
 const c = 221;
 const d = c - 5;
 
 console.log(`d = ${d} c = ${c}`);
+*/
 
 /* String
 const what = "changyong";
@@ -27,4 +29,11 @@ const wat = 6666;
 
 /* float
 const wat = 55.1;
+*/
+
+/* Array
+const something = "something"
+const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", true, something];
+console.log(daysOfWeek);
+console.log(daysOfWeek[2]);
 */
